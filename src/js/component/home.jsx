da9fd486ea4b598b03c5ac577ad641fad168edd4
Daniel Martinez <d.martinez74@gmail.com>
2023-04-01 01:59:31 +0000

    Simple counter using React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+
+
+const myDivStyle = {
+	background: "black",
+	height: "75px",
+	width: "auto",
+	display: "flex",
+	flexDirection: "row"
+}
+
+const letterStyle = {
+	color: "white",
+	marginLeft: "50px",
+	fontSize: "50px",
+}
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div style={myDivStyle}>
+			<div style={letterStyle}><i className="fa-regular fa-clock"></i></div>
+			<div style={letterStyle} id="numberSix">{props.numberSix}</div>
+			<div style={letterStyle} id="numberFive">{props.numberFive}</div>
+			<div style={letterStyle} id="numberFour">{props.numberFour}</div>
+			<div style={letterStyle} id="numberThree">{props.numberThree}</div>
+			<div style={letterStyle} id="numberTwo">{props.numberTwo}</div>
+			<div style={letterStyle} id="numberOne">{props.numberOne}</div>
 		</div>
 	);
 };
+
+Home.propTypes = {
+	numberOne: PropTypes.number,
+	numberTwo: PropTypes.number,
+	numberThree: PropTypes.number,
+	numberFour: PropTypes.number,
+	numberFive: PropTypes.number,
+	numberSix: PropTypes.number,
+}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,50 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let numberOne = 0;
+let numberTwo = 0;
+let numberThree = 0;
+let numberFour = 0;
+let numberFive = 0;
+let numberSix = 0;
+
+setInterval(function counter() {
+    numberOne++
+    if (numberOne > 9) {
+        numberTwo++;
+        numberOne = 0;
+    }
+    if (numberTwo > 5) {
+        numberThree++;
+        numberTwo = 0;
+    }
+    if (numberThree > 9) {
+        numberFour++;
+        numberThree = 0;
+    }
+    if (numberFour > 5) {
+        numberFive++;
+        numberFour = 0;
+    }
+    if (numberFive > 9) {
+        numberSix++;
+        numberFive = 0;
+    }
+    if (numberSix > 9) {
+        numberSix++;
+    }
+
+    ReactDOM.render(<Home numberOne={numberOne}
+        numberTwo={numberTwo}
+        numberThree={numberThree}
+        numberFour={numberFour}
+        numberFive={numberFive}
+        numberSix={numberSix} />, document.querySelector("#app"));
+        
+    console.log(numberSix, numberFive, numberFour, numberThree, numberTwo, numberOne)
+}, 1000)
+
+
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
